1aebe0f611edcb3ccecc550b86a1f4dba069058a
piotrekvs <pvanselow.2000@poczta.fm>
2024-03-17 15:19:58 +0100

    Add project 1 fix 2

--- project1/matrix_multiplication_strassen.py
+++ project1/matrix_multiplication_strassen.py
@@ -1,58 +1,97 @@
 from typing import List
 
+fp_operations = 0
 
-def multiply_strassen(A: List[List[int]], B: List[List[int]]) -> List[List[int]]:
-    rows_A = len(A)
-    cols_A = len(A[0])
-    rows_B = len(B)
-    cols_B = len(B[0])
+class MatrixMultiplier():
+    def __init__(self) -> None:
+        self.threshold: int
+        self.fp_operations: int
 
-    if cols_A != rows_A or cols_A != rows_B or cols_A != cols_B:
-        raise Exception("Err: matrices should be squared and of the same size\n")
+    def multiply(self, A: List[List[int]], B: List[List[int]], threshold_power: int) -> List[List[int]]:
+        self.threshold = 2 ** threshold_power
+        self.fp_operations = 0
+        rows_A = len(A)
+        cols_A = len(A[0])
+        rows_B = len(B)
+        cols_B = len(B[0])
+
+        if cols_A != rows_A or cols_A != rows_B or cols_A != cols_B:
+            raise Exception("Err: matrices should be squares and of the same size\n")
+        
+        return self._multiply_recurrent(A, B)
     
-    return multiply_strassen_recurrent(A, B)
-
-def add_matrix(A: List[List[int]], B:  List[List[int]]) -> List[List[int]]:
-    return [[A[y][x] + B[y][x] for x in range(len(A[0]))] for y in range(len(A))] 
-
-def subtract_matrix(A: List[List[int]], B:  List[List[int]]) -> List[List[int]]:
-    return [[A[y][x] - B[y][x] for x in range(len(A[0]))] for y in range(len(A))] 
-
-def multiply_strassen_recurrent(A: List[List[int]], B: List[List[int]]) -> List[List[int]]:
-    l = len(A)
-    C = [[0 for _ in range(l)] for _ in range(l)] 
+        
+    def _multiply_recurrent(self, A: List[List[int]], B: List[List[int]]) -> List[List[int]]:
+        if self.threshold >= len(A):
+            self.fp_operations += len(A) ** 3
+            return self.multiply_basic(A, B)
+        else:
+            return self._multiply_strassen_recurrent(A, B)
     
-    if (l == 1): 
-        C[0][0] = A[0][0] * B[0][0] 
+
+    def _multiply_strassen_recurrent(self, A: List[List[int]], B: List[List[int]]) -> List[List[int]]:
+        l = len(A)
+        C = [[0 for _ in range(l)] for _ in range(l)] 
+        
+        if (l == 1): 
+            C[0][0] = A[0][0] * B[0][0] 
+            return C
+        
+        mid_idx = l // 2
+        A00 = [[A[y][x] for x in range(mid_idx)] for y in range(mid_idx)] 
+        A01 = [[A[y][x + mid_idx]  for x in range(mid_idx)] for y in range(mid_idx)] 
+        A10 = [[A[mid_idx + y][x]  for x in range(mid_idx)] for y in range(mid_idx)] 
+        A11 = [[A[y + mid_idx][x + mid_idx]  for x in range(mid_idx)] for y in range(mid_idx)] 
+        B00 = [[B[y][x] for x in range(mid_idx)] for y in range(mid_idx)] 
+        B01 = [[B[y][x + mid_idx] for x in range(mid_idx)] for y in range(mid_idx)] 
+        B10 = [[B[mid_idx + y][x] for x in range(mid_idx)] for y in range(mid_idx)] 
+        B11 = [[B[y + mid_idx][x + mid_idx] for x in range(mid_idx)] for y in range(mid_idx)] 
+
+        P1 = self._multiply_recurrent(self.add_matrix(A00, A11), self.add_matrix(B00, B11))
+        P2 = self._multiply_recurrent(self.add_matrix(A10, A11), B00)
+        P3 = self._multiply_recurrent(A00, self.subtract_matrix(B01, B11))
+        P4 = self._multiply_recurrent(A11, self.subtract_matrix(B10, B00))
+        P5 = self._multiply_recurrent(self.add_matrix(A00, A01), B11)
+        P6 = self._multiply_recurrent(self.subtract_matrix(A10, A00), self.add_matrix(B00, B01))
+        P7 = self._multiply_recurrent(self.subtract_matrix(A01, A11), self.add_matrix(B10, B11))
+
+        C00 = self.add_matrix(self.subtract_matrix(self.add_matrix(P1, P4), P5), P7)
+        C01 = self.add_matrix(P3, P5)
+        C10 = self.add_matrix(P2, P4)
+        C11 = self.add_matrix(self.add_matrix(self.subtract_matrix(P1, P2), P3), P6)
+
+        for i in range(mid_idx): 
+            for j in range(mid_idx): 
+                C[i][j] = C00[i][j] 
+                C[i][j + mid_idx] = C01[i][j] 
+                C[mid_idx + i][j] = C10[i][j] 
+                C[i + mid_idx][j + mid_idx] = C11[i][j] 
         return C
     
-    mid_idx = l // 2
-    A00 = [[A[y][x] for x in range(mid_idx)] for y in range(mid_idx)] 
-    A01 = [[A[y][x + mid_idx]  for x in range(mid_idx)] for y in range(mid_idx)] 
-    A10 = [[A[mid_idx + y][x]  for x in range(mid_idx)] for y in range(mid_idx)] 
-    A11 = [[A[y + mid_idx][x + mid_idx]  for x in range(mid_idx)] for y in range(mid_idx)] 
-    B00 = [[B[y][x] for x in range(mid_idx)] for y in range(mid_idx)] 
-    B01 = [[B[y][x + mid_idx] for x in range(mid_idx)] for y in range(mid_idx)] 
-    B10 = [[B[mid_idx + y][x] for x in range(mid_idx)] for y in range(mid_idx)] 
-    B11 = [[B[y + mid_idx][x + mid_idx] for x in range(mid_idx)] for y in range(mid_idx)] 
 
-    P1 = multiply_strassen(add_matrix(A00, A11), add_matrix(B00, B11))
-    P2 = multiply_strassen(add_matrix(A10, A11), B00)
-    P3 = multiply_strassen(A00, subtract_matrix(B01, B11))
-    P4 = multiply_strassen(A11, subtract_matrix(B10, B00))
-    P5 = multiply_strassen(add_matrix(A00, A01), B11)
-    P6 = multiply_strassen(subtract_matrix(A10, A00), add_matrix(B00, B01))
-    P7 = multiply_strassen(subtract_matrix(A01, A11), add_matrix(B10, B11))
+    def multiply_basic(self, A: List[List[int]], B: List[List[int]]) -> List[List[int]]:
+        rows_A = len(A)
+        cols_A = len(A[0])
+        rows_B = len(B)
+        cols_B = len(B[0])
 
-    C00 = add_matrix(subtract_matrix(add_matrix(P1, P4), P5), P7)
-    C01 = add_matrix(P3, P5)
-    C10 = add_matrix(P2, P4)
-    C11 = add_matrix(add_matrix(subtract_matrix(P1, P2), P3), P6)
+        if cols_A != rows_B:
+            raise Exception("Err: col(A) must be equal to row(B)\n")
 
-    for i in range(mid_idx): 
-        for j in range(mid_idx): 
-            C[i][j] = C00[i][j] 
-            C[i][j + mid_idx] = C01[i][j] 
-            C[mid_idx + i][j] = C10[i][j] 
-            C[i + mid_idx][j + mid_idx] = C11[i][j] 
-    return C
+        C = [[0] * cols_B for _ in range(rows_A)]
+
+        for row in range(rows_A):
+            for col in range(cols_B):
+                for i in range(rows_A):
+                    C[row][col] += A[row][i] * B[i][col]
+        return C
+    
+    
+    def add_matrix(self, A: List[List[int]], B:  List[List[int]]) -> List[List[int]]:
+        self.fp_operations += len(A) ** 2
+        return [[A[y][x] + B[y][x] for x in range(len(A[0]))] for y in range(len(A))] 
+
+
+    def subtract_matrix(self, A: List[List[int]], B:  List[List[int]]) -> List[List[int]]:
+        self.fp_operations += len(A[0]) * len(A)
+        return [[A[y][x] - B[y][x] for x in range(len(A[0]))] for y in range(len(A))] 
--- project1/test_matrix_multiplication.py
+++ project1/test_matrix_multiplication.py
@@ -1,45 +1,96 @@
 # type: ignore
 import timeit
-from typing import List
-from matrix_multiplication_basic import multiply_basic
-from matrix_multiplication_strassen import multiply_strassen
+from matrix_multiplication_strassen import MatrixMultiplier
 import matplotlib.pyplot as plt
 from random import randint
 
-def print_matrix(A: List[List[int]]):
+def print_matrix(A):
     for i in range(len(A)): 
         print(A[i], end = "\n")
     print()
 
 
-def generate_random_square_matrix(n: int, max: int = 100) -> List[List[int]]:
+def generate_random_square_matrix(n: int, max: int = 100):
     return [[randint(-max, max) for _ in range(n)] for _ in range(n)] 
 
-iterations = 9
-iteration_list = list(range(0, iterations))
 
-def test():
-    basic_times = [0.0] * iterations
-    strassen_times = [0.0] * iterations
-    for i in iteration_list:
+mx_multiplier = MatrixMultiplier()
+
+def test_times():
+    iterations = 11
+    times = [0.0] * iterations
+    for i in range(0, iterations):
         print(f'iteration = {i}')
         A = generate_random_square_matrix(2**i)
         B = generate_random_square_matrix(2**i)
         
-        basic_times[i] = timeit.timeit(lambda: multiply_basic(A, B), number=1)
-        strassen_times[i] = timeit.timeit(lambda: multiply_strassen(A, B), number=1)
+        times[i] = timeit.timeit(lambda: mx_multiplier.multiply(A, B, 4), number=1)
 
-    print(basic_times)
-    print(strassen_times)
-
-    plt.plot(basic_times, color='red', label='Basic')
-    plt.plot(strassen_times, color='blue', label='Strassen')
+    plt.plot(times, color='blue')
     plt.legend()
-    plt.xlabel('Matrix size')
+    plt.xlabel('Matrix size (power of 2)')
     plt.ylabel('Execution Time (seconds)')
     plt.show()
 
-test()
+def test_thresholds():
+    thresholds = 9
+    times = [0.0] * thresholds
+
+    for i in range(0, thresholds):
+        print(f'iteration = {i}')
+        A = generate_random_square_matrix(2**8)
+        B = generate_random_square_matrix(2**8)
+        
+        times[i] = timeit.timeit(lambda: mx_multiplier.multiply(A, B, i), number=1)
+
+    plt.plot(times, color='blue')
+    plt.legend()
+    plt.title("Matrix size 2^8")
+    plt.xlabel('Threshold (power of 2)')
+    plt.ylabel('Execution Time (seconds)')
+    plt.show()
+
+
+def test_times():
+    iterations = 10
+    th4 = [0.0] * iterations
+    th6 = [0.0] * iterations
+    th8 = [0.0] * iterations
+    for i in range(0, iterations):
+        print(f'iteration = {i}')
+        A = generate_random_square_matrix(2**i)
+        B = generate_random_square_matrix(2**i)
+
+        mx_multiplier.multiply(A, B, 4)
+        th4[i] = mx_multiplier.fp_operations
+
+        mx_multiplier.multiply(A, B, 6)
+        th6[i] = mx_multiplier.fp_operations
+
+        mx_multiplier.multiply(A, B, 8)
+        th8[i] = mx_multiplier.fp_operations
+
+    plt.plot(th4, color='blue')
+    plt.plot(th6, color='green')
+    plt.plot(th8, color='red')
+    plt.legend()
+    plt.xlabel('Matrix size (power of 2)')
+    plt.ylabel('Floating point operations')
+    plt.show()
+
+
+def test_multiplication():
+    A = generate_random_square_matrix(2**6)
+    B = generate_random_square_matrix(2**6)
+    C_strassen = mx_multiplier.multiply(A, B, 4)
+    C_basic = mx_multiplier.multiply_basic(A, B)
+    for row in range(len(C_strassen)):
+        for col in range(len(C_strassen)):
+            if C_strassen[row][col] != C_basic[row][col]:
+                print("error!")
+    print("success!")
+
+test_multiplication()
 
 
 
